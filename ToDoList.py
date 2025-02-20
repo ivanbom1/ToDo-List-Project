80773aa8a704cc9af4e7f9629ef1789210ff7760
Ivan Bomko <ivan.bomko@epita.fr>
2025-02-20 14:31:41 +0100

    tick function added

--- ToDoList.py
+++ ToDoList.py
@@ -19,7 +19,11 @@ class ToDoList:
 
     def show_tasks(self):
         for task in self.tasks:
-            print(f"Title: {task.title}, Description: {task.description}, Completed: {task.completed}, Date: {task.date}")
+            if task.completed:
+                c_status = "✅"
+            else:
+                c_status = "❌"
+            print(f"Title: {task.title}, Description: {task.description}, Completed: {c_status}, Date: {task.date}")
 
     def add_task(self, title, description, completed=False):
         new_task = Task(title, description, completed)
@@ -30,14 +34,25 @@ class ToDoList:
             if task.title == title:
                 self.tasks.remove(task)
 
+    def sort_tasks(self):
+        self.tasks.sort(key=lambda task: task.date, reverse=False)
+
+    def tick_task(self, title):
+        for task in self.tasks:
+            if task.title == title:
+                task.completed = True
+
+
 todo_list = ToDoList()
 
 todo_list.add_task("Do my HW", "French class homework", False)
 
 todo_list.add_task("Laundry", "Do my laundry", False)
 
+todo_list.add_task("Algebra", "Last assignment", False)
+
 todo_list.show_tasks()
 
-todo_list.remove_task("Do my HW")
+todo_list.tick_task("Algebra")
 
 todo_list.show_tasks()
